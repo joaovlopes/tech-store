fc35ba2d724ffb80ef2327591b2c6121344cb476
João Lopes <joaovlopesmartins@gmail.com>
2024-02-02 18:54:55 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@
 import { useSession } from "next-auth/react";
 
 import Image from "next/image"
+import Categories from "./components/categories";
 
 export default function Home() {
   const { data } = useSession();
@@ -16,5 +17,9 @@ export default function Home() {
       sizes="100vw"
       alt="Até 55% de desconto esse mês!"
     />
+
+    <div className="mt-8">
+      <Categories />
+    </div>
   </div>
 }
