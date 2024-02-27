29dbb4e8ac8537ebf54f33dd09881fae4b930a5b
João Lopes <joaovlopesmartins@gmail.com>
2024-02-27 03:41:36 -0300

    feat: ensure product can be removed from cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,13 +1,20 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import Image from "next/image"
 import { Button } from "./button";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
     product: CartProduct
 }
 
 const CartItem = ({product}: CartItemProps) => {
+    const {decreaseProductQuantity} =  useContext(CartContext)
+
+    const handleDecreaseProductQuantityClick = () => {
+        decreaseProductQuantity(product.id)
+    }
+
     return (
         <div className="flex items-center justify-between">
             <div className="flex items-center gap-4">
@@ -34,7 +41,7 @@ const CartItem = ({product}: CartItemProps) => {
 
                     <div className="flex items-center gap-2">
                         <Button size="icon" variant="outline" className="w-8 h-8">
-                            <ArrowLeftIcon size={16}/>
+                            <ArrowLeftIcon size={16} onClick={handleDecreaseProductQuantityClick}/>
                         </Button>
 
                         <span className="text-xs">{product.quantity}</span>
