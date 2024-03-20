752a48447201eb73c04c9ac0e5261559821d6ec4
João Lopes <joaovlopesmartins@gmail.com>
2024-03-20 19:14:55 -0300

    chore: fixing deployment error for vercel to prisma-adapter typescript

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prismaClient),
